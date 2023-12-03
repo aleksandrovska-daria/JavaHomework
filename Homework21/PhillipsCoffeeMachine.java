95a7327ed4103eb7d0cad12c51bf7f6c5bcd619f
aleksandrovska-daria <148548974+aleksandrovska-daria@users.noreply.github.com>
2023-12-03 21:37:10 +0200

    Add files via upload

---
+++ Homework21/PhillipsCoffeeMachine.java
@@ -0,0 +1,18 @@
+package Homework21;
+
+public class PhillipsCoffeeMachine extends CoffeeMachine{
+
+    public PhillipsCoffeeMachine() {
+        super(3000, 700, 2400);
+    }
+    public PhillipsCoffeeMachine(int currentWaterLevel, int currentCoffeeLevel, int currentGarbageLevel) {
+        super(currentWaterLevel, currentCoffeeLevel, currentGarbageLevel);
+    }
+
+    @Override
+    public void addWater() {
+    }
+    @Override
+    public void addCoffee() {
+    }
+}
---
+++ Homework21/SiemensCoffeeMachine.java
@@ -0,0 +1,19 @@
+package Homework21;
+
+public class SiemensCoffeeMachine extends CoffeeMachine {
+
+
+    public SiemensCoffeeMachine() {
+        super(2000, 500, 2000);
+    }
+
+    public SiemensCoffeeMachine(int currentWaterLevel, int currentCoffeeLevel, int currentGarbageLevel) {
+        super(currentWaterLevel, currentCoffeeLevel, currentGarbageLevel);
+    }
+    @Override
+    public void addWater() {
+    }
+    @Override
+    public void addCoffee() {
+    }
+}
